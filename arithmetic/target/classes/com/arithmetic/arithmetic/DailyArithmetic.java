d88bfbfb81134ac5699ed801f13184d508f87934
shangpengyang <13783170243@163.com>
2021-03-03 19:49:46 +0800

    算法更新

--- arithmetic/target/classes/com/arithmetic/arithmetic/DailyArithmetic.java
+++ arithmetic/target/classes/com/arithmetic/arithmetic/DailyArithmetic.java
@@ -5,14 +5,15 @@ import java.util.Arrays;
 public class DailyArithmetic {
 	/**
 	 * 双赛道，每次只能容纳两匹马比赛，现有12匹马，采用最优算法获取前三名
-	 * 时间复杂度O(n^2)
+	 * 冒泡排序：时间复杂度O(n^2)
 	 */
 	public static void horseRacing(){
 		int[] a={96,93,30,60,19,95,19,83,24,5,88,100};
+		int	b;
 		for(int i=0;i<a.length;i++){
 			for(int i1=0;i1<a.length;i1++){
 			if(a[i]<a[i1]){
-			int	b=a[i1];
+				b=a[i1];
 				a[i1]=a[i];
 				a[i]=b;
 			}
@@ -22,6 +23,6 @@ public class DailyArithmetic {
 		
 	}
     public static void main(String args[]){
-    	DailyArithmetic.horseRacing();
+     new DailyArithmetic().horseRacing();
     }
 }
