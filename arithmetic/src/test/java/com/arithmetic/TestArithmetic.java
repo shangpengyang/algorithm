1478c67bf35729ecddecdcfbe69fa62b57bd8a83
shangpengyang <13783170243@163.com>
2021-03-03 21:19:09 +0800

    算法提交

---
+++ arithmetic/src/test/java/com/arithmetic/TestArithmetic.java
@@ -0,0 +1,13 @@
+package com.arithmetic;
+
+import org.junit.Test;
+
+import com.arithmetic.arithmetic.DailyArithmetic;
+
+public class TestArithmetic {
+	@Test
+	public void  arithmeticTest(){
+		new DailyArithmetic().horseRacing();
+	}
+
+}
