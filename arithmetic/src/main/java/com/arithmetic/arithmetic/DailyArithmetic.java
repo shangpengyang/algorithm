818fb0d7fe28e42826e624638bced9158b561a44
shangpengyang <13783170243@163.com>
2021-03-26 17:55:50 +0800

    直接插入排序

--- arithmetic/src/main/java/com/arithmetic/arithmetic/DailyArithmetic.java
+++ arithmetic/src/main/java/com/arithmetic/arithmetic/DailyArithmetic.java
@@ -1,6 +1,7 @@
 package com.arithmetic.arithmetic;
 
 import java.util.Arrays;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class DailyArithmetic {
 	//循环次数
@@ -29,9 +30,9 @@ public class DailyArithmetic {
 		System.out.println(Arrays.toString(a)); 
 		
 	}
-	//快速排序
+	//快速排序，最优时间复杂度log(N),平均时间复杂度log(N),最差时间复杂度n^2,算法不稳定
 	public static int[] fastSort(int[] a,int start,int end){
-		//int[] a={96,93,30,60,19,95,19,83,24,5,88,100};
+		//int[] a={96,93,30,60,19,95,19,83,24,5,100,88};
 		int i=start;
 		int j=end;
 		int pointValue=a[i];
@@ -55,35 +56,37 @@ public class DailyArithmetic {
 	     }
 	    
 		}
-		 if(i-1>start) a=fastSort(a,start,i-1);
-	     if(j+1<end) a=fastSort(a,j+1,end);
-		//System.out.println("快速排序循环次数"+count);
-		//System.out.println(Arrays.toString(a));
+		 if(i-1>start)
+	     a=fastSort(a,start,i-1);
+	     if(j+1<end) 
+	     a=fastSort(a,j+1,end);
         return a;
 
-		/*for(int i=0;i<a.length;i++){
-			for(int i1=0;i1<a.length;i1++){
-				if(a[i1]<a[i]&&i1>i){
-				int x=a[i];
-				a[i]=a[i1];
-				a[i1]=x;
-				count++;
-				}
-				if(a[i1]>a[i]&&i1<i){
-				int x=a[i];
-				a[i]=a[i1];
-				a[i1]=x;
-				count++;
-				}
-			}
-			
-		}*/
- 
-
+		
 	}
+	//直接插入排序，最优时间复杂度O(N),平均时间复杂度O(N^2),最差时间复杂度O(N^2),算法稳定
+		public static int[] insertSort(int[] a){
+			int count=0;
+			for(int i=0;i<a.length;i++){
+			count++;
+			int	j=i;
+			int temp=a[j];
+			while(j>0&&temp<a[j-1]){
+					count++;
+					a[j]=a[j-1];
+					j--;
+				}
+				a[j]=temp;
+			}
+			System.out.println("插入排序循环次数"+count);
+			System.out.println(Arrays.toString(a));
+			return a;
+		}
     public static void main(String args[]){
      new DailyArithmetic().maopaoSort();
-     System.out.println("快速排序循环次数"+count);
-   System.out.println(Arrays.toString(new DailyArithmetic().fastSort(new int[]{96,93,30,60,19,95,19,83,24,5,88,100},0,11)));
+     new DailyArithmetic().insertSort(new int[]{96,93,30,60,19,95,19,83,24,5,100,88});
+   System.out.println(Arrays.toString(new DailyArithmetic().fastSort(new int[]{96,93,30,60,19,95,19,83,24,5,100,88},0,11)));
+   System.out.println("快速排序循环次数"+count);
+
     }
 }
